ca4626420cb310ac49f6b9657c0f9d6d173d9c55
Olamicode <olalekanf@decagonhq.com>
2022-02-05 12:57:54 +0100

    Setup bottomNavView with navController Modified themes and string resources

--- app/src/main/java/com/decagonhq/varensuitest/MainActivity.kt
+++ app/src/main/java/com/decagonhq/varensuitest/MainActivity.kt
@@ -2,10 +2,28 @@ package com.decagonhq.varensuitest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.decagonhq.varensuitest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.apply {
+            bottomNavView.setupWithNavController(navController)
+        }
     }
 }
